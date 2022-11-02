d6f0189a3034ff87eb775d81f49421030511abca
7leem <116731471+7leemOfficial@users.noreply.github.com>
2022-11-02 19:52:14 +0300

    main.py
    My code. You can view it on any site that runs python code for you. (e.g., Trinket, Replit, etc.)

---
+++ RPS vs AI..py
@@ -0,0 +1,6 @@
+﻿# Start Code Here
+import time
+Commands = ["!help", "!rock", "!paper", "!scissors", "!r", "!p", "!s", "!random", 1, 2, 3, "!quit"]
+print("Hello, there player! Welcome to RPS! In this game, you need to enter commands such as help, rock, paper, scissors, etc. Try and beat the AI!")
+while not Player_command == Commands[11]:
+  Player_command = str(input("Enter a command. Type !help for more info."))
